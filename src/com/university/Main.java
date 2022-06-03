a5813db7db41e5d2e2bb6e5eba0dce61b1b2c7e3
X3NON-135 <immort4ld3momo@gmail.com>
2022-06-03 16:43:37 +0300

    first commit

---
+++ src/com/university/Main.java
@@ -0,0 +1,15 @@
+package com.university;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final int SIZE = 13;
+        for (int j = 1; j <= SIZE; j++)
+            System.out.print("*");
+
+        System.out.print("\n*Hello world*\n");
+
+        for (int j = 1; j <= SIZE; j++)
+            System.out.print("*");
+    }
+}
